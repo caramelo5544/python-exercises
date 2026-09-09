5aa19045b53886b0c8e494890daeb357442b3f24
caramelo5544 <caramel5.5@outlook.com>
2026-09-10 01:56:24 +0300

    restore caesar

--- ex3.py
+++ ex3.py
@@ -1,2 +1,8 @@
-import random
-random.randint(1, 10)
+text = input("Text (small letters): ")
+shift = int(input("Shift: "))
+result = ""
+for letter in text:
+    new = chr((ord(letter) - 97 + shift) % 26 + 97)
+    result = result + new
+print("Encrypted:", result)
+input("Press Enter to exit")
