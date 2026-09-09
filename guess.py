0372638378b4881c26923e175d01665630cad15b
caramelo5544 <caramel5.5@outlook.com>
2026-09-10 01:41:43 +0300

    add guessing game

---
+++ guess.py
@@ -0,0 +1,15 @@
+import random
+
+secret = random.randint(1, 20)
+tries = 0
+
+while True:
+    guess = int(input("Guess (1-20): "))
+    tries = tries + 1
+    if guess < secret:
+        print("Higher!")
+    elif guess > secret:
+        print("Lower!")
+    else:
+        print("Correct! Tries:", tries)
+        break
